085db11ea02ba58e8f09f9843b0cc7342cff1595
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-18 13:19:04 +0100

    Ex 04 Tableaux Listes Enum

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabEx04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shopping = new List<string>();
+            shopping.Add("Carottes");
+            shopping.Add("Oignons");
+            shopping.Add("Patates");
+            shopping.Add("Salade");
+            shopping.Add("Tomates");
+
+            foreach (string shop in shopping)
+            {
+                Console.WriteLine(shop);
+            }
+        }
+        
+    }
+}
